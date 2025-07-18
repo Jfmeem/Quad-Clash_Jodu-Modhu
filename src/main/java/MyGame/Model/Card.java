84ab5f0ed2d1c8d1a777bf9a4a82ed86ebd34bfb
Jfmeem <jfmeem187@gmail.com>
2025-07-18 16:58:49 +0600

    initial

--- src/main/java/MyGame/Model/Card.java
+++ src/main/java/MyGame/Model/Card.java
@@ -1,4 +1,26 @@
 package MyGame.Model;
 
 public class Card {
+    public enum Suits{
+        DevilQueen, Emberlore, Valora, WhispersOfWings
+    }
+    private final Suits suit;
+    private final String imagePath;
+
+    public Card(Suits suit, String imagePath){
+        this.suit = suit;
+        this.imagePath = imagePath;
+
+    }
+
+    public Suits getSuit(){
+        return suit;
+    }
+    public String getImagePath(){
+        return imagePath;
+    }
+
+    public String toString(){
+        return "Card {suit = " + suit + ", imagePath = ' " + imagePath  + "}";
+    }
 }
