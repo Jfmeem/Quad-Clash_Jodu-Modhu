302c16473b26084b6f5e11a246f3f8d4936248e2
nabila1644-iit <bsse1644@iit.du.ac.bd>
2025-08-01 14:58:49 +0600

    updated--

--- src/main/java/com/example/quadclashjodumodhu/MyGame/Model/Deck.java
+++ src/main/java/com/example/quadclashjodumodhu/MyGame/Model/Deck.java
@@ -1,4 +1,30 @@
 package com.example.quadclashjodumodhu.MyGame.Model;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 public class Deck {
+    private final List<Card>cards;
+    public Deck(){
+        cards=new ArrayList<>();
+        initializeDeck();
+    }
+    private void initializeDeck(){
+        for (Card.Suits suit : Card.Suits.values()){
+            for (int i=0;i<4;i++){
+                String imagePath="/images/cards"+suit.name().toLowerCase() +"_"+(i+1) +" .pnj";
+                cards.add(new Card(suit,imagePath));
+            }
+        }
+    }
+    public void shuffle(){
+        Collections.shuffle(cards);
+    }
+    public Card drawCard(){
+        if(cards.isEmpty())return null;
+        return cards.remove(0);
+    }
+    public boolean isEmpty(){
+        return cards.isEmpty();
+    }
+
 }
--- src/main/java/com/example/quadclashjodumodhu/MyGame/Model/HumanPlayer.java
+++ src/main/java/com/example/quadclashjodumodhu/MyGame/Model/HumanPlayer.java
@@ -1,4 +1,11 @@
 package com.example.quadclashjodumodhu.MyGame.Model;
 
-public class HumanPlayer {
+public class HumanPlayer extends Player {
+    public HumanPlayer(String name){
+        super(name);
+    }
+    @Override
+    public Card chooseCardToPlay(){
+        return null;
+    }
 }
